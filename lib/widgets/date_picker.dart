15faec11fce7379129fee4d366cd1cc90232d3a1
Singgih Putra <singgihputrap@gmail.com>
2022-12-08 16:08:43 +0700

    add value transformer

--- lib/widgets/date_picker.dart
+++ lib/widgets/date_picker.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:expiry/utils/constant.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
@@ -33,6 +34,7 @@ class AppDatePicker extends StatelessWidget {
           decoration: InputDecoration(
             hintText: title,
           ),
+          valueTransformer: (value) => Timestamp.fromDate(value ?? DateTime.now()),
         ),
         kVerticalLargeBox,
       ],
