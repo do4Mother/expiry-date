8ad0b713769ee067b34c09139aec192bd426b675
Singgih Putra <singgihputrap@gmail.com>
2022-12-11 11:00:25 +0700

    add MockStorageRepository

--- test/helper/repositories.dart
+++ test/helper/repositories.dart
@@ -1,8 +1,10 @@
-import 'package:expiry/models/profile.dart';
 import 'package:expiry/repositories/product_repository.dart';
 import 'package:expiry/repositories/profile_repository.dart';
+import 'package:expiry/repositories/storage_repository.dart';
 import 'package:mocktail/mocktail.dart';
 
 class MockProfileRepository extends Mock implements ProfileRepository {}
 
 class MockProductRepository extends Mock implements ProductRepository {}
+
+class MockStorageRepository extends Mock implements StorageRepository {}
