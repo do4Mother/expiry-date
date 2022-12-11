f93c3330f902777e46e898b8ccead85dddadf395
Singgih Putra <singgihputrap@gmail.com>
2022-12-11 11:09:17 +0700

    emit GetListProduct when completed AuthenticationBloc

--- lib/views/home/home.dart
+++ lib/views/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:expiry/utils/constant.dart';
+import 'package:expiry/utils/state_helper.dart';
 import 'package:expiry/views/home/bloc/list_product/list_product_bloc.dart';
 import 'package:expiry/views/home/widgets/no_items.dart';
 import 'package:expiry/views/product/add_product/add_product.dart';
@@ -6,6 +7,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../app/bloc/authentication/authentication_bloc.dart';
+import '../../models/profile.dart';
 import '../../widgets/product.dart';
 
 class HomeView extends StatefulWidget {
@@ -22,60 +25,69 @@ class _HomeViewState extends State<HomeView> {
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
     final productState = context.watch<ListProductBloc>().state;
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Expiry Date'),
-        // leading: IconButton(
-        //   onPressed: () {},
-        //   icon: const Icon(Icons.menu),
-        // ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () => context.push(AddProductView.routeName),
-      ),
-      body: NestedScrollView(
-        headerSliverBuilder: (context, innerBoxIsScrolled) {
-          return [
-            SliverToBoxAdapter(
-              child: productState.builder(
-                loaded: Visibility(
-                  visible: productState.data?.isNotEmpty ?? false,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      kVerticalMediumBox,
-                      Padding(
-                        padding: kPaddingItemView,
-                        child: Text(
-                          'List of items',
-                          style: textTheme.headline4,
-                        ),
-                      )
-                    ],
+    return BlocListener<AuthenticationBloc, StateHelper<Profile>>(
+      listener: (context, state) {
+        state.listener(
+          loaded: () {
+            context.read<ListProductBloc>().add(GetListProduct());
+          }
+        );
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Expiry Date'),
+          // leading: IconButton(
+          //   onPressed: () {},
+          //   icon: const Icon(Icons.menu),
+          // ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () => context.push(AddProductView.routeName),
+        ),
+        body: NestedScrollView(
+          headerSliverBuilder: (context, innerBoxIsScrolled) {
+            return [
+              SliverToBoxAdapter(
+                child: productState.builder(
+                  loaded: Visibility(
+                    visible: productState.data?.isNotEmpty ?? false,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        kVerticalMediumBox,
+                        Padding(
+                          padding: kPaddingItemView,
+                          child: Text(
+                            'List of items',
+                            style: textTheme.headline4,
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-          ];
-        },
-        body: productState.builder(
-          loaded: Visibility(
-            visible: productState.data?.isNotEmpty ?? false,
-            replacement: const NoItems(),
-            child: GridView.builder(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                childAspectRatio: 8 / 11,
-                crossAxisSpacing: 8,
+            ];
+          },
+          body: productState.builder(
+            loaded: Visibility(
+              visible: productState.data?.isNotEmpty ?? false,
+              replacement: const NoItems(),
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 8 / 11,
+                  crossAxisSpacing: 8,
+                ),
+                padding: kPaddingListView,
+                itemCount: productState.data?.length ?? 0,
+                itemBuilder: (context, index) {
+                  return AppProduct(
+                    product: productState.data![index],
+                  );
+                },
               ),
-              padding: kPaddingListView,
-              itemCount: productState.data?.length ?? 0,
-              itemBuilder: (context, index) {
-                return AppProduct(
-                  product: productState.data![index],
-                );
-              },
             ),
           ),
         ),
