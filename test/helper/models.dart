11a0967b92ccbccbd0ac9bbd9d17fb86daa1c9c5
Singgih Putra <singgihputrap@gmail.com>
2022-12-11 11:00:15 +0700

    add FakeProduct and FakeFile

--- test/helper/models.dart
+++ test/helper/models.dart
@@ -1,4 +1,11 @@
+import 'dart:io';
+
+import 'package:expiry/models/product.dart';
 import 'package:expiry/models/profile.dart';
 import 'package:mocktail/mocktail.dart';
 
 class FakeProfile extends Fake implements Profile {}
+
+class FakeProduct extends Fake implements Product {}
+
+class FakeFile extends Fake implements File {}
